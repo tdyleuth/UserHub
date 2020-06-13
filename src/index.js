11e2c33605d84ac543cf0395caa04392fce3d066
tdyleuth <tony.dyleuth@gmail.com>
2020-06-13 10:40:18 -0700

    UserHub:React Edition: Day 1

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  Header,
+  UserPosts
+} from './components';
+
+import {
+  getUsers,
+  getPostsByUser
+} from './api';
+
+const App = () => {
+  const [userList, setUserList] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        setUserList(users)
+      })
+      .catch(error => {
+       
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!currentUser) {
+      setUserPosts([]);
+      return;
+    }
+
+    getPostsByUser(currentUser.id)
+      .then(posts => {
+        setUserPosts(posts);
+      })
+      .catch(error => {
+       
+      });
+  }, [currentUser]);
+
+  return (
+    <div id="App">
+      <Header
+        userList={ userList }
+        currentUser={ currentUser }
+        setCurrentUser={ setCurrentUser } />
+      {
+        currentUser
+        ? <UserPosts
+            userPosts={ userPosts }
+            currentUser={ currentUser } />
+        : null
+      }
+
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
